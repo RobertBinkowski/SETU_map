163a108d9783a2c916f4d55ce5aaca5a24fa98b2
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-01-22 19:18:22 +0000

    Header Fixed

--- PHP/index.php
+++ PHP/index.php
@@ -5,21 +5,6 @@ include "./src/ErrorHandler.php";
 include "./src/AutoLoader.php";
 include "./src/header.php";
 
-//Handle Exceptions
-set_error_handler("ErrorHandler::handleError");
-set_exception_handler("ErrorHandler::handleException");
-
-//Set all headers to return Json type
-header("Content-type:application/json; charset=UTF-8");
-
-//Database Connection
-$host = "127.0.0.1";
-$username = "root";
-$password = "pass";
-$database = "setu_map";
-
-$database = new Database($host, $database, $username, $password);
-
 $request = explode("/", $_SERVER["REQUEST_URI"]);
 
 // if($request[1] == ""){
