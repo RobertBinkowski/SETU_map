9c92d028fe751a44259e93127a21354a90263335
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-02-04 18:22:02 +0000

    2D/3D array for the a*

--- js/a_star.js
+++ js/a_star.js
@@ -3,85 +3,46 @@
  *  by:             Robert Binkowski
  *  Student No:     C00237917
  */
-// import { Node } from "./oldNode";
-/**
- *
- * Heuristic function to calculate the heuristic value between 2 points
- *
- * @param {Node} start - start Node
- * @param {Node} end - end Node
- * @returns combined difference between 2 points
- */
-function calculateHeuristic(start, end) {
-  //Calculate the distance between 2 nodes
-  let d1 = Math.abs(start.x - end.x);
-  let d2 = Math.abs(start.y - end.y);
-  let d3 = Math.abs(start.z - end.z);
 
-  //Return the outcome which is a heuristic value
-  return Math.abs(d1 + d2 + d3);
-}
-
-/**
- *
- * @param {Node} cameFrom - Previous Node/s
- * @param {Node} current - Current Node
- */
-function reconstructPath(previous, current) {
-  const path = { current };
-  while (current !== previous.key) {
-    current = previous[current];
-    path.prepend(current);
-  }
-  return path;
-}
+import { Node } from "./node.js";
 
 let cols = 5;
 let rows = 5;
+let hight = 2;
+
+let start; //starting grid point
+let end; // ending grid point (goal)
+
+let openSet = []; //array containing unevaluated grid points
+let closedSet = []; //array containing completely evaluated grid points
+
 let grid = new Array(cols);
 
-function Node(x, y) {
-  this.x = x;
-  this.y = y;
-  this.f = 0;
-  this.g = 0;
-  this.h = 0;
-  this.neighbors = [];
-  this.parent = undefined;
-
-  // update neighbors array for a given grid point
-  this.updateNeighbors = function (grid) {
-    let i = this.x;
-    let j = this.y;
-    if (i < cols - 1) {
-      this.neighbors.push(grid[i + 1][j]);
-    }
-    if (i > 0) {
-      this.neighbors.push(grid[i - 1][j]);
-    }
-    if (j < rows - 1) {
-      this.neighbors.push(grid[i][j + 1]);
-    }
-    if (j > 0) {
-      this.neighbors.push(grid[i][j - 1]);
-    }
-  };
-}
-
 function createMap(rows = 5, cols = 5) {
+  let output = "";
+
+  //Create 2D Array
   for (let i = 0; i < cols; i++) {
     grid[i] = new Array(rows);
+    // Create a 3D Array
+    // for (let ii = 0; ii < hight; ii++) {
+    //   grid[i][ii] = new Array(hight);
+    // }
   }
 
+  //Populate Arrays
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-      grid[i][j] = new Node(i, j);
+      grid[i][j] = new Node(i + j, i, j);
+      output += "[" + j + "," + i + "]";
     }
+    output += "\n";
   }
 
+  //Update Neighbors
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-      grid[i][j].updateNeighbors(grid);
+      grid[i][j].updateNeighbors(grid, rows, cols);
     }
   }
   start = grid[0][0];
@@ -89,52 +50,7 @@ function createMap(rows = 5, cols = 5) {
 
   openSet.push(start);
 
-  console.log(grid);
+  return output;
 }
 
 console.log(createMap());
-
-// // A* finds a path from start to goal.
-// // h is the heuristic function. h(n) estimates the cost to reach goal from node n.
-// export function A_Star(start, goal, h){
-//     // The set of discovered nodes that may need to be (re-)expanded.
-//     // Initially, only the start node is known.
-//     // This is usually implemented as a min-heap or priority queue rather than a hash-set.
-//     openSet := {start}
-
-//     // For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start
-//     // to n currently known.
-//     cameFrom := an empty map
-
-//     // For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
-//     gScore := map with default value of Infinity
-//     gScore[start] := 0
-
-//     // For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our current best guess as to
-//     // how cheap a path could be from start to finish if it goes through n.
-//     fScore := map with default value of Infinity
-//     fScore[start] := h(start)
-
-//     while (openSet > 0){
-//         // This operation can occur in O(Log(N)) time if openSet is a min-heap or a priority queue
-//         current := the node in openSet having the lowest fScore[] value
-//         if (current == goal){
-//             return reconstructPath(cameFrom, current);
-//         }
-//         openSet.Remove(current)
-//         foreach ( neighbor as current){
-//             // d(current,neighbor) is the weight of the edge from current to neighbor
-//             // tentative_gScore is the distance from start to the neighbor through current
-//             tentative_gScore := gScore[current] + d(current, neighbor)
-//             if tentative_gScore < gScore[neighbor]
-//                 // This path to neighbor is better than any previous one. Record it!
-//                 cameFrom[neighbor] := current
-//                 gScore[neighbor] := tentative_gScore
-//                 fScore[neighbor] := tentative_gScore + h(neighbor)
-//                 if neighbor not in openSet
-//                     openSet.add(neighbor)
-//         }
-//     }
-//     // Open set is empty but goal was never reached
-//     return failure
-// }
---
+++ js/node.js
@@ -0,0 +1,75 @@
+/**
+ *  Title:          Priority Queue
+ *  by:             Robert Binkowski
+ *  Student No:     C00237917
+ */
+
+/**
+ *
+ * @param {str} name - Name of the location
+ * @param {int} priority - priority
+ * @param {double} x - X Coordinates
+ * @param {double} y - Y Coordinates
+ * @param {double} z - Z Coordinates
+ * @param {str} info - Location Information
+ * @param {str} acronym - Acronym of the location
+ * @param {str} type - Type of the location [room, bathroom] - Pulled from database
+ * @param {int} floor - floor
+ * @param {double} geo_longitude - Longitude Geo Location
+ * @param {double} geo_latitude - Latitude Geo Location
+ *
+ */
+export function Node(
+  name,
+  x,
+  y,
+  z = 0,
+  info = "",
+  acronym = "",
+  type = "",
+  floor = 0,
+  geo_longitude = null,
+  geo_latitude = null
+) {
+  this.name = name;
+
+  //Location Information
+  this.info = info;
+  this.acronym = acronym;
+  this.type = type;
+  this.floor = floor;
+
+  //Location Info
+  this.x = x;
+  this.y = y;
+  this.z = z;
+  this.blocked = false;
+
+  this.f = 0; //Total Cost Function
+  this.g = 0; //From the start
+  this.h = 0; //heuristic
+
+  this.neighbors = []; // neighbors of the current point
+  this.parent = undefined; // source of the current point
+
+  this.geo_longitude = geo_longitude;
+  this.geo_latitude = geo_latitude;
+
+  // update neighbors array for a given grid point
+  this.updateNeighbors = function (grid, row, column) {
+    let i = this.x;
+    let j = this.y;
+    if (i < column - 1) {
+      this.neighbors.push(grid[i + 1][j]);
+    }
+    if (i > 0) {
+      this.neighbors.push(grid[i - 1][j]);
+    }
+    if (j < row - 1) {
+      this.neighbors.push(grid[i][j + 1]);
+    }
+    if (j > 0) {
+      this.neighbors.push(grid[i][j - 1]);
+    }
+  };
+}
