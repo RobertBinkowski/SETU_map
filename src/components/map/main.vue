08cd4f9e050bdbdfec24e89f2c4099e4b572f14d
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-02-10 15:44:06 +0000

    small adjustments

---
+++ src/components/map/main.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>{{ info }}</section>
+</template>
+
+<script>
+  import axios from "axios";
+  // export default {
+  //   created() {
+  //     const success = (position) => {
+  //       const latitude = position.coords.latitude;
+  //       const longitude = position.coords.longitude;
+
+  //       // var x = document.getElementById("location");
+  //       // x.innerHTML = "Latitude: " + latitude + "<br>Longitude: " + longitude;
+  //       // Do something with the position
+  //     };
+
+  //     const error = (err) => {
+  //       console.log(error);
+  //     };
+  //     // This will open permission popup
+  //     navigator.geolocation.getCurrentPosition(success, error);
+  //   },
+  // };
+  export default {
+    data() {
+      return {
+        info: null,
+      };
+    },
+    mounted() {
+      axios
+        .get("http://localhost:8000/room")
+        .then((response) => (this.info = response));
+    },
+  };
+</script>
