09f69d93145ac3a3272410de839f77d3865e9e49
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-02-17 18:24:38 +0000

    displayed data

--- Vue/src/components/map/main.vue
+++ Vue/src/components/map/main.vue
@@ -1,19 +1,27 @@
 <template>
-  <section>{{ data }}</section>
+  <h2>Map2</h2>
+  <section v-for="entry in entries" :key="entry.id">
+    <p>
+      {{ entry }}
+    </p>
+  </section>
 </template>
 
 <script>
   import axios from "axios";
+  import { ref } from "vue";
 
   export default {
     name: "App",
     setup() {
+      let entries = ref([]);
       async function getData() {
         const { data } = await axios.get("http://localhost:8000/user");
-        return data;
+        console.log(data);
+        entries.value = data;
       }
-      const data = getData();
-      return { data };
+      getData();
+      return { entries };
     },
   };
 </script>
