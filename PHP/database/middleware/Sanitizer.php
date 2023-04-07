ef01a465b076ddcb83e04529fe2973b0c08f8742
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-04-07 19:45:21 +0100

    Updated Password Sanitizer

--- PHP/database/middleware/Sanitizer.php
+++ PHP/database/middleware/Sanitizer.php
@@ -1,31 +1,74 @@
 <?php
 
-function sanitizePassword($password)
+function sanitizePassword(string $password): string | bool
 {
-    // Set minimum and maximum password length
-    $minLength = 8;
-    $maxLength = 64;
+
+    // Check length
+    if (!checkSize($password)) {
+        return false;
+    }
 
     // Remove specific unwanted characters
     $unwantedChars = array('"', "'", '\\', '<', '>');
     $password = str_replace($unwantedChars, '', $password);
 
-    // Check if the password meets the length requirements
-    $passwordLength = strlen($password);
-    if ($passwordLength < $minLength || $passwordLength > $maxLength) {
-        throw new Exception("Password must be between $minLength and $maxLength characters long.");
-    }
+    // Remove whitespace from the password
+    $password = trim($password);
+
+    // Convert the password to lowercase for case-insensitive comparison
+    $password = strtolower($password);
 
     return $password;
 }
 
-function hashPassword($password)
+function hashPassword(string $password): string
 {
-    $salt = "SomeSaltHere";
+    // Hash the password with Bcrypt
+    $password = password_hash($password, PASSWORD_BCRYPT);
 
-    $password += $salt;
-
-    $password = password_hash($password, PASSWORD_DEFAULT);
-
+    // Return the hashed password
     return $password;
+}
+
+function comparePasswords(string $password, string $input): bool
+{
+    // check if set to null
+    if ($input == null || $password == null) {
+        return false;
+    }
+    // check if set to empty strings
+    if ($input = "" || $password == "") {
+        return false;
+    }
+
+    // Check size
+    if (!checkSize($password)) {
+        return false;
+    }
+
+    // Confirm hashes match 
+    if (password_verify($input, $password)) {
+        return true;
+    }
+
+    // Default Value is false
+    return false;
+}
+
+/**
+ * Check The length of the password to match the length
+ */
+function checkSize(string $input): bool
+{
+
+    // Set minimum and maximum password length
+    $min = 8;
+    $max = 64;
+
+    // Check if the password meets the length requirements
+    if (strlen($input) < $min || strlen($input) > $max) {
+        return false;
+    }
+
+    return true;
 }
